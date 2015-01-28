db2962a224839d2ce98aa70d504c50d0348c43b1
Lee Blue <lee@reality66.com>
2015-01-28 12:30:28 -0500

    Add optional post excerpt to product listing grid view

--- templates/content-product-grid-item.php
+++ templates/content-product-grid-item.php
@@ -1,11 +1,18 @@
 <li class="cc-product-grid-item">
 
     <div class="cc-product-grid-image-container">
-        <?php the_post_thumbnail( 200, array( 'class' => 'cc-grid-item-image'  ) ); ?>
+        <a href="<?php echo get_permalink(); ?>" title="<?php the_title() ?>"><?php the_post_thumbnail( 200, array( 'class' => 'cc-grid-item-image'  ) ); ?></a>
     </div>
 
     <p class="cc-product-grid-title"><?php the_title(); ?></p>
 
-    <a class="cc-button-primary" href="<?php echo get_permalink(); ?>"><?php _e( 'View Details', 'cart66' ); ?></a>
+    <?php 
+        $post = get_post();
+        if ( ! empty( $post->post_excerpt ) ) :
+    ?>
+    <p class="cc-product-grid-excerpt"><?= $post->post_excerpt ?></p>
+    <?php endif; ?>
+
+    <a class="cc-button-primary" href="<?php echo get_permalink(); ?>" title="<?php the_title(); ?>"><?php _e( 'View Details', 'cart66' ); ?></a>
 
 </li>
