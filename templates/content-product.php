44930b6d96a7d1c97b956aebbfd0c869290628a3
Lee Blue <lee@reality66.com>
2015-02-19 16:21:08 -0500

    Use vertical product forms on product pages

--- templates/content-product.php
+++ templates/content-product.php
@@ -19,7 +19,7 @@
     <div class="cc-product-form">
         <?php
             $product_sku = get_post_meta( get_the_ID(), '_cc_product_sku', true );
-            echo do_shortcode( '[cc_product sku="' . $product_sku . '" quantity="true" price="true"]' );                    
+            echo do_shortcode( '[cc_product sku="' . $product_sku . '" quantity="true" price="true" display="vertical" ]' );                    
         ?>
     </div>
 </div>
